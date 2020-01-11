13ef8c6ca74c534994200252a3d2dea015f8b4c2
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-11 21:57:57 +0300

    Updates

---
+++ precache-manifest.b98afb2773238c3a8f119d6fb4b9d19d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69016c5f3f5fe6b50612472d67e6a6c9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b34aa67a354727012c7b",
+    "url": "/portfolio/static/css/2.b6481b72.chunk.css"
+  },
+  {
+    "revision": "ff716bba2ac1b9607ba1",
+    "url": "/portfolio/static/css/main.fe50dda3.chunk.css"
+  },
+  {
+    "revision": "b34aa67a354727012c7b",
+    "url": "/portfolio/static/js/2.c2052553.chunk.js"
+  },
+  {
+    "revision": "ff716bba2ac1b9607ba1",
+    "url": "/portfolio/static/js/main.dbfd87bf.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  },
+  {
+    "revision": "4401d3c975d7f37568eb4098b006f0d4",
+    "url": "/portfolio/static/media/Api.4401d3c9.jpg"
+  },
+  {
+    "revision": "5b015ae22d35a8421166b11aed9beaa1",
+    "url": "/portfolio/static/media/me.5b015ae2.jpg"
+  },
+  {
+    "revision": "b04f7e86c15857340ae653fc3dfad0aa",
+    "url": "/portfolio/static/media/pci.b04f7e86.jpg"
+  },
+  {
+    "revision": "65065756305ef471a130aeae780d92a4",
+    "url": "/portfolio/static/media/portfolio.65065756.png"
+  },
+  {
+    "revision": "a4e5df0f8182c13065098b3ccbd872cf",
+    "url": "/portfolio/static/media/questioner.a4e5df0f.png"
+  }
+]);
