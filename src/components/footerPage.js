70873762c74bbe44fb6271376dc13e5a80045e1f
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-13 14:28:00 +0300

    added footer

--- src/components/footerPage.js
+++ src/components/footerPage.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import { BackTop } from 'antd';
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import { BackTop } from "antd";
+import Github from './images/github-image-796x418.png';
+import LinkedIN from './images/linkedin.png'
 
 class Footer extends Component {
-    render() {
-        return(
-            <div>
-                <BackTop />
-            </div>
-        );
-    }
+  render() {
+    const backStyle = {
+      backgroundColor: "#f2f2f2"
+    };
+    return (
+      <div style={backStyle}>
+        <Container className="Footer">
+          <BackTop />
+          <h6>JONATHAN MUSILA @2020</h6>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Footer;
